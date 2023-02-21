d11760d650fe088fdc659e983eb3dd91d81bbfc3
lingaiahdanda <lingaiahdanda@gmail.com>
2023-02-21 11:51:36 -0600

    Add project files.

---
+++ Controllers/PokemonController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PokemonReviewApp.Interfaces;
+using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PokemonController : Controller
+    {
+        private readonly IPokemonRepository _pokemonRepository;
+
+        public PokemonController(IPokemonRepository pokemonRepository)
+        {
+            _pokemonRepository = pokemonRepository;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(200,Type = typeof(IEnumerable<Pokemon>))]
+
+        public IActionResult GetPokemons()
+        {
+            var pokemons = _pokemonRepository.GetPokemons();
+
+            //ModelState is like when you send the wrong data to API, this model states finds that 
+            //you sent the worng data
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            //return the result
+            return Ok(pokemons);
+        }
+    }
+}
---
+++ Models/Country.cs
@@ -0,0 +1,11 @@
+﻿namespace PokemonReviewApp.Models
+{
+    public class Country
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        // one to many relationship
+        public ICollection<Owner> Owners { get; set; }
+    }
+}
